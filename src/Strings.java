cc5cbe22d64dd9b80ce7526940231088e6eefa3c
phoebelam <pzjl00@student.cis.edu.hk>
2023-02-17 08:21:09 +0800

    Completed

--- src/Strings.java
+++ src/Strings.java
@@ -1,27 +1,26 @@
 public class Strings {
-
     public static void main(String[] args) {
 
         // 1.
-
+int age = 33;
         // 2.
-
+String name = "T Swizzle";
         // 3.
-
+System.out.println(age);
         // 4.
-
+System.out.println(name);
         // 5.
-
+String description = "My name is " + name + " and I am " + age + " years old.";
         // 6.
-
+System.out.println(description);
         // 7.
-
+System.out.println(description.length());
         // 8.
-
+String nameSubDescription = description.substring(11,20);
         // 9.
-
+int ageSubDescription = Integer.parseInt(description.substring(30,32));
         // 10.
-
+String subDescription = "My name is " + nameSubDescription + " and I am " + ageSubDescription + " years old.";
     }
 
 }
